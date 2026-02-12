78c20ecb1a7764df483743131b4176fdbf6277e6
zhoraf1 <georgiyfrol@gmail.com>
2026-02-12 23:19:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "chrome-extension://ijgkbcbalaekboipcmaefchfjpognmog/saver/vk.com/177413/ecco2k%20-%20peroxide%20%5B_nightcorebot%5D.mp3", // Music streaming URL
+        musicUrl: "https://cs9-2v4.vkuseraudio.net/s/v1/ac/xD-5iptT1ZzTIC15G3bYq7Uum9hPfay-mZOakuTKeOxPMXPcojRVV_adsV192SxLJ3gUSsMAEllf3eseY6BH10oiMXEHkhnWb5_M7G6BF1ExYqkrSS9gYrhIX_bveq3_B_80t5Qur9-cyXt0wGGk9GpFwDpbjR5HHQGiK63nryIsjP8/index.m3u8?siren=1", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
